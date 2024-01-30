5e12f7e399b172617f6cab7a4310d68c261fc2e4
Fir35t0rm <30534256+fir35t0rm@users.noreply.github.com>
2024-01-30 11:26:20 +0800

    Add files via upload

---
+++ Import-Users.ps1
@@ -0,0 +1,67 @@
+﻿Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+ 
+#Check Script is running with Elevated Privileges
+Check-RunAsAdministrator
+
+# Path to CSV file
+$csvPath = "D:\AD-Import\AD-Import.csv"
+
+# Import CSV and iterate through each row
+$users = Import-Csv $csvPath
+foreach ($user in $users) {
+    # Extract data from CSV
+    
+    $firstname = $user.givenName
+    $lastname = $user.sn
+    $passwordprefix = "SRT-" + $user.password
+    $password = ConvertTo-SecureString $passwordprefix -AsPlainText -Force
+    # SKIP $department = $user.department
+    # SKIP $manager = $user.managedby
+    # SKIP $phone = $user.telephoneNumber
+    $username = $firstname + "." + $lastname
+    $name = $firstname + " " + $lastname
+    $ouPath = $user.OUPath
+
+    # Create User
+    New-ADUser `
+    -Name $name `
+    -SamAccountName $username `
+    -UserPrincipalName "$username@FORCE.internal" `
+    -GivenName $firstname `
+    -Surname $lastname `
+    -DisplayName $name `
+    -AccountPassword $password -Enabled $true `
+    -Path $ouPath
+}
+
+Read-Host -Prompt "Script completed, please check each error of Added AD accounts
+
+Press any key to close window"
---
+++ change-PW.ps1
@@ -0,0 +1,76 @@
+﻿Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+ 
+#Check Script is running with Elevated Privileges
+Check-RunAsAdministrator
+
+$OUmatch = "CertII","CertII_VETiS","CertIII","CertIV_Networking","CertIV_WebDevelopment","Diploma"
+
+while ($true) {
+    $OUprompt = Read-Host -Prompt "Please enter the following OU to change all users in OU
+- CertII
+- CertII_VETiS
+- CertIII
+- CertIV_Networking
+- CertIV_WebDevelopment
+- Diploma
+Type 'quit' to cancel"
+    if ( $OUprompt -match 'quit') {
+    Exit
+    }
+    elseif ( $OUprompt -match 'Lecturers' ) {
+    Write-Host "
+This OU is not allowed!
+" -ForegroundColor red -BackgroundColor white
+    }
+    elseif ( $OUprompt -in $OUmatch) {
+    break
+    }
+    else {
+    Write-Host "Invalid option" -ForegroundColor red -BackgroundColor white
+    }
+}
+
+$Passwordprompt = Read-Host -Prompt "Enter desired Password for affect users in $OUprompt"
+ 
+# Set the distinguished name of the OU
+$ouPath = "OU=" + $OUprompt + ",OU=Users,OU=Bunbury,OU=FORCE,DC=FORCE,DC=INTERNAL"  # Replace with your OU's distinguished name
+
+# Get all users in the specified OU
+$users = Get-ADUser -Filter * -SearchBase "$ouPath"
+
+# Loop through each user and set a new password
+foreach ($user in $users) {
+    $newPassword = ConvertTo-SecureString -String $Passwordprompt -AsPlainText -Force  # Replace "NewPassword123!" with the desired password
+    Set-ADAccountPassword -Identity $user -NewPassword $newPassword -Reset
+}
+Read-Host -Prompt "Script completed, please check for errors
+
+Press any key to close window"
